86b2975100d3bc56d842b3b458612729cba32046
Imasha Nethmi <165567075+ImashaNethmi@users.noreply.github.com>
2025-03-28 08:42:35 +0530

    Node.js API Delete Movie

--- netflix-api/controllers/UserController.js
+++ netflix-api/controllers/UserController.js
@@ -1,6 +1,6 @@
 const User = require("../models/UserModel");
 
-module.exports.addToLikeMovies = async (requestAnimationFrame, res) => {
+module.exports.addToLikedMovies = async (req, res) => { 
     try{
         const { email, data} = req.body;
         const user = await User.findOne({ email});
@@ -25,9 +25,9 @@ module.exports.addToLikeMovies = async (requestAnimationFrame, res) => {
     }
 };
 
-module.exports.getLikedMovies =  async(req,res) => {
+module.exports.getLikedMovies =  async(req, res) => {
     try{
-        const { email} = req.body;
+        const { email} = req.body ;
         const user = await User.findOne({ email});
         if(user) {
             res.json({msg: "success", movies: user.likedMovies});
@@ -35,5 +35,29 @@ module.exports.getLikedMovies =  async(req,res) => {
 
     }catch (err){
         return res.json({ msg: "Error fetching movie"});
+    }
+};
+
+module.exports.removeFromLikedMovies = async(req,res) => {
+    try {
+        const { email, movieId} = req.body;
+        const user = await User.findOne({email})
+        if (user) {
+            const { likedMovies} = user;
+            const movieIndex = likedMovies.findIndex(({ id }) => id === movieId)
+            if(!movieIndex ) res.status(400).send({msg:"Movie not found"})
+                likedMovies.splice(movieIndex,1);
+                await User.findByIdAndUpdate(
+                    user._id,
+                {
+                    likedMovies,
+                },               
+                    {new: true }                
+             );  
+            }   
+            return res.json({msg:"Movie Deleted",movies:likedMovies})
+    }catch(err) {
+        console.log(err);
+        return res.json({ msg: "Error deleting movie"});
     }
 }
